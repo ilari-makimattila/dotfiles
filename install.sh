b9958580dabe01a9ab79f5cd7df0c30b16d89e96
Ilari Mäkimattila <ilari@auterion.com>
2018-07-20 08:55:48 +0200

    Add initial version of install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+link() {
+  file="$1"
+  dotfile="$DIR/dotfiles/$file"
+  target="$HOME/$file"
+
+  if [ -h "$target" ]
+  then
+      echo "'$target' already installed"
+  elif [ -a "$target" ]
+  then
+      echo "'$target' exists, will not override"
+  else
+      echo "installing '$target'"
+      ln -s "$dotfile" "$target"
+  fi
+}
+
+for f in $(find "$DIR/dotfiles" -maxdepth 1 | tail -n +2)
+do
+    file=$(basename "$f")
+    echo "$f = $file"
+    link "$file"
+done
+
