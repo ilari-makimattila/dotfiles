a346eaf4f540be0eb43e41b85590b3f5bff99f72
Ilari Mäkimattila <ilari.makimattila@gmail.com>
2021-03-01 15:00:39 +0100

    Improve vhost helper

--- profile.d/evenito.sh
+++ profile.d/evenito.sh
@@ -1,10 +1,10 @@
 function print-vhosts() {
-  docker inspect $(docker ps |rg evenito | awk '{print $1}') | jq '.[] | .Config.Env | .[]' -r | rg VIRTUAL_HOST | sed 's/VIRTUAL_HOST=//'
+  docker inspect $(docker ps --format "{{.ID}}") | jq '.[] | .Config.Env | .[]' -r | grep VIRTUAL_HOST | sed 's/VIRTUAL_HOST=//'
 }
 
 function update-vhosts() {
-  for vhost in $(print-vhosts);
+  for vhost in $(print-vhosts)
   do
-    rg -q $vhost /etc/hosts || echo "127.1.0.1 $vhost" | sudo tee -a /etc/hosts
+    grep -q $vhost /etc/hosts || echo "127.1.0.1 $vhost" | sudo tee -a /etc/hosts
   done
 }
